02b23412ff4a02bc79e9ca91f19773700e0f90d7
Ente <goost17@arcor.de>
2022-01-07 16:23:28 +0100

    make menuconfig

--- script/updatemcu.sh
+++ script/updatemcu.sh
@@ -9,7 +9,7 @@ cd ~/klipper
 echo "Start update mcu rpi"
 echo ""
 make clean KCONFIG_CONFIG=/home/pi/klipper_config/script/config.host
-make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.host
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.host
 make -j $CORES KCONFIG_CONFIG=/home/pi/klipper_config/script/config.host
 read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.host
@@ -20,7 +20,7 @@ echo ""
 echo "Start update mcu XYZE"
 echo ""
 make clean KCONFIG_CONFIG=/home/pi/klipper_config/script/config.trigorilla
-make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.trigorilla
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.trigorilla
 make -j $CORES KCONFIG_CONFIG=/home/pi/klipper_config/script/config.trigorilla
 read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 make flash FLASH_DEVICE=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
